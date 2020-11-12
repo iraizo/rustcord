6ac186e9dc6bd02e8a493447f5db784c3ad90d44
Ashley <admin@trapacid.icu>
2020-11-12 18:06:09 -0500

    basic window with text

--- src/gui.rs
+++ src/gui.rs
@@ -1,3 +1,30 @@
 /* 
     Usage: everything that needs rendering goes in here
 */
+use iced::{Element, Sandbox, Settings, Text};
+
+pub fn draw_window() {
+    Window::run(Settings::default())
+}
+
+struct Window;
+
+impl Sandbox for Window {
+    type Message = ();
+
+    fn new() -> Window {
+        Window
+    }
+
+    fn title(&self) -> String {
+        String::from("test")
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // This application has no interactions
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("test").into()
+    }
+}
