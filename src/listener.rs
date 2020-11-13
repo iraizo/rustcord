47cf91cbbab47df5b0584f3ba339a291370c1c6a
iraizo <groebner.loron@gmail.com>
2020-11-13 20:08:08 +0100

    ✨ Message listener

--- src/listener.rs
+++ src/listener.rs
@@ -1,3 +1,21 @@
 /*
     usage: threads that keep listening to events like on_message
 */
+
+pub mod listener {
+    use serenity::{
+        async_trait,
+        model::{channel::Message, gateway::Ready},
+        prelude::*,
+    };
+
+    struct Handler;
+
+    #[async_trait]
+    impl EventHandler for Handler {
+        async fn message(&self, ctx: Context, msg: Message) {}
+        /* add more handlers here */ 
+
+        /* https://docs.rs/serenity/0.9.1/serenity/client/trait.EventHandler.html#method.message */
+    }
+}
