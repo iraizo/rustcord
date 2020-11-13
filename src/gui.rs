ea18cbef3020ebfd7ef79eb86392f6a97c101416
Michał Minarowski <neoney@pm.me>
2020-11-13 01:41:49 +0100

    ✨  Add some reactivity tests to the GUI

--- src/gui.rs
+++ src/gui.rs
@@ -1,30 +1,53 @@
 /* 
     Usage: everything that needs rendering goes in here
 */
-use iced::{Element, Sandbox, Settings, Text};
+use iced::{button, Element, Button, Sandbox, Settings, Text, Column, Align};
 
 pub fn draw_window() {
-    Window::run(Settings::default())
+    Window::run(Settings::default()) 
 }
 
-struct Window;
+#[derive(Default)]
+struct Window {
+    value: i32,
+    increment_button: button::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+}
 
 impl Sandbox for Window {
-    type Message = ();
+    type Message = Message;
 
     fn new() -> Window {
-        Window
+        Window::default()
     }
 
     fn title(&self) -> String {
-        String::from("test")
+        String::from("Rustcord GUI test")
     }
 
-    fn update(&mut self, _message: Self::Message) {
-        // This application has no interactions
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+        }
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("test").into()
+        Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(
+        Button::new(&mut self.increment_button, Text::new("Increment"))
+                    .on_press(Message::IncrementPressed),
+            )
+            .push(
+            Text::new(self.value.to_string()).size(50)
+            )
+            .into()
     }
 }
